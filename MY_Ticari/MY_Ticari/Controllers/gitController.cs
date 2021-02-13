7619ba5e32ab34b9584f2d86fef31af62a302366
GÖRKEM ÖZTÜRK <20181011140@std.nisantasi.edu.tr>
2021-02-13 08:14:21 +0300

    testing

---
+++ MY_Ticari/MY_Ticari/Controllers/gitController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MY_Ticari.Controllers
+{
+    public class gitController : Controller
+    {
+        // GET: git
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
